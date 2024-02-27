5306da6106bb9023a4e13fd8793360722fafea85
asharma2026 <m265766@usna.edu>
2024-02-27 14:47:54 -0500

    percent difference

--- pandasforcsv.py
+++ pandasforcsv.py
@@ -5,6 +5,7 @@ from datacleaning import clean_dataframe
 df = pd.read_csv('FF_SR_ data.csv')
 df = clean_dataframe(df)
 
+
 SRmean = df['SR Mean per 100g'].astype(float)
 FFmean = df['FF Mean per 100g'].astype(float)
 
@@ -18,18 +19,15 @@ df['difference'] = (SRmean-FFmean)
 std = df['difference'].std()
 #outliers = df[(df['difference'] > (3*std)) | (df['difference'] < -(3*std))]
 sorted = df.sort_values(['rank', 'difference'], ascending = [True, False])
-df2 = sorted[['rank','difference',]]
+df2 = sorted[['rank','difference','FF Mean per 100g']]
+print(df2)
 rank = df2.groupby('rank').sum().reset_index()
 df3 = rank.merge(vc,on='rank').fillna(0)
-df3['mean'] = df3['difference']/df3['count']
-outliers = df3[(df3['mean'] > std) | (df3['mean'] < -std)]
+print(df3)
+df3['percent difference'] = abs(df3['difference']/df3['FF Mean per 100g'])*100
+outliers = df3[(df3['percent difference'] > std) | (df3['percent difference'] < -std)]
 print(outliers)
 
-#print(first)
-#cols = first['FF Food description'],first['difference']
-##print(cols)
-# fig = px.histogram(df, x=df['FF Food description'],y=df['difference'])
-# fig.show()
-fig = px.scatter(df3, x=df3['mean'],y=df3['rank'],orientation = 'h')
+fig = px.scatter(df3, x=df3['percent difference'],y=df3['rank'],orientation = 'h')
 fig.show()
 
